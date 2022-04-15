3da3dae8a573cb6f5c3a38b5e709e44240567cb4
Supakrit Kongkam <62160143@go.buu.ac.th>
2022-04-16 02:42:46 +0700

    updated Database booking

--- models/booking.js
+++ models/booking.js
@@ -5,7 +5,7 @@ const bookingSchema = Schema({
   endTime: Date,
   approveres: [{ type: Schema.Types.ObjectId, ref: 'User', approveDate: Date, status: Number }],
   result_status: Number,
-  order: [{ type: Schema.Types.ObjectId, ref: 'Approve' }],
+  order: { type: Schema.Types.ObjectId, ref: 'Approve' },
   user_id: { type: Schema.Types.ObjectId, ref: 'User' },
   room_id: { type: Schema.Types.ObjectId, ref: 'Room' }
 }, {

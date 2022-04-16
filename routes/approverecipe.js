ab5597ed03967a80f8e8c39b7b926fb4e1300716
ภูบดินทร์ สุขเจริญ <62160295@go.buu.ac.th>
2022-04-16 18:03:42 +0700

    Fix data at approvercipe

--- routes/approverecipe.js
+++ routes/approverecipe.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const ApproveRecipe = require('../models/approvesrecipe')
+const Approve = require('../models/approve')
 
 const getApproveRecipe = async function (req, res, next) {
   try {
-    const approveRecipe = await ApproveRecipe.find({}).exec()
+    const approveRecipe = await ApproveRecipe.find({}).populate().exec()
     res.status(200).json(approveRecipe)
   } catch (err) {
     return res.status(500).send({
@@ -15,13 +16,15 @@ const getApproveRecipe = async function (req, res, next) {
 
 const getApproveRecipeid = async function (req, res, next) {
   const id = req.params.id
+  const ObjectId = require('mongoose').Types.ObjectId
   try {
-    const approveRecipe = await ApproveRecipe.findById(id).exec()
+    const approveRecipe = await ApproveRecipe.find({ user_id: new ObjectId(id) }).populate({ path: 'user_id' }).populate({ path: 'booking_id', populate: { path: 'room_id user_id' } }).exec()
     if (approveRecipe === null) {
       return res.status(404).json({
         message: 'ApproveRecipe not found!!'
       })
     }
+    console.log(approveRecipe)
     res.json(approveRecipe)
   } catch (err) {
     return res.status(404).json({
@@ -31,13 +34,14 @@ const getApproveRecipeid = async function (req, res, next) {
 }
 
 const addApproveRecipe = async function (req, res, next) {
+  const approve = await Approve.findById(req.body.booking.approve_id)
   const newApproveRecipe = new ApproveRecipe({
-    booking_id: req.body.booking_id,
+    booking_id: req.body.bookingid,
     status_approve: 0,
     current_order: 1,
-    approve_id: req.body.approve_id,
+    approve_id: req.body.booking.approve_id,
     timeApprove: null,
-    user_id: req.body.user_id
+    user_id: approve.approver1
   })
   try {
     await newApproveRecipe.save()
@@ -48,6 +52,7 @@ const addApproveRecipe = async function (req, res, next) {
     })
   }
 }
+
 router.get('/', getApproveRecipe)
 router.get('/:id', getApproveRecipeid)
 router.post('/addApproveRecipe', addApproveRecipe)
